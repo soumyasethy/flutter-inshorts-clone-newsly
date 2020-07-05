4b68b16f409368704d7e8a0b5f9eccec56a5f65f
Soumya Ranjan Sethy <soumyaranjan.sethy@bounceshare.com>
2020-07-05 15:27:57 +0530

    more refactor

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
